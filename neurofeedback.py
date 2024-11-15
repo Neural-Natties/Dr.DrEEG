81ae5b4dd75d6c9985a734f28c0cfef617147077
Nandan <nandanramesh123@gmail.com>
2024-11-14 23:46:37 -0700

    feat: stream out PPG, ACC, and GYRO data

--- neurofeedback.py
+++ neurofeedback.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Estimate Relaxation from Band Powers
-
-This example shows how to buffer, epoch, and transform EEG data from a single
-electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
-Furthermore, it shows how ratios of the band powers can be used to estimate
-mental state for neurofeedback.
-
-The neurofeedback protocols described here are inspired by
-*Neurofeedback: A Comprehensive Review on System Design, Methodology and Clinical Applications* by Marzbani et. al
-
-Adapted from https://github.com/NeuroTechX/bci-workshop
+Comprehensive Muse S Monitor
+Displays EEG, PPG, Accelerometer, and Gyroscope data in real-time
 """
 
-import numpy as np  # Module that simplifies computations on matrices
-import matplotlib.pyplot as plt  # Module used for plotting
-from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
-import utils  # Our own utility functions
-
-# Handy little enum to make code more readable
+import numpy as np
+import matplotlib.pyplot as plt
+from pylsl import StreamInlet, resolve_byprop
+import utils
 
 
 class Band:
@@ -28,162 +17,192 @@ class Band:
     Beta = 3
 
 
-""" EXPERIMENTAL PARAMETERS """
-# Modify these to change aspects of the signal processing
-
-# Length of the EEG data buffer (in seconds)
-# This buffer will hold last n seconds of data and be used for calculations
+# Experimental parameters
 BUFFER_LENGTH = 5
-
-# Length of the epochs used to compute the FFT (in seconds)
 EPOCH_LENGTH = 1
-
-# Amount of overlap between two consecutive epochs (in seconds)
 OVERLAP_LENGTH = 0.8
-
-# Amount to 'shift' the start of each next consecutive epoch
 SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
-
-# Index of the channel(s) (electrodes) to be used
-# 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
 INDEX_CHANNEL = [1]
 
 if __name__ == "__main__":
-
-    """1. CONNECT TO EEG STREAM"""
-
-    # Search for active LSL streams
+    # Connect to EEG stream
     print("Looking for an EEG stream...")
     streams = resolve_byprop("type", "EEG", timeout=2)
     if len(streams) == 0:
         raise RuntimeError("Can't find EEG stream.")
 
-    # Set active EEG stream to inlet and apply time correction
     print("Start acquiring data")
     inlet = StreamInlet(streams[0], max_chunklen=12)
     eeg_time_correction = inlet.time_correction()
 
-    # Get the stream info and description
+    # Get stream info
     info = inlet.info()
     description = info.desc()
-
-    # Get the sampling frequency
-    # This is an important value that represents how many EEG data points are
-    # collected in a second. This influences our frequency band calculation.
-    # for the Muse 2016, this should always be 256
     fs = int(info.nominal_srate())
 
-    """ 2. INITIALIZE BUFFERS """
-
-    # Initialize raw EEG data buffer
+    # Initialize buffers
     eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
-    filter_state = None  # for use with the notch filter
+    ppg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 5))
+    acc_buffer = np.zeros((int(fs * BUFFER_LENGTH), 5))
+    gyro_buffer = np.zeros((int(fs * BUFFER_LENGTH), 5))
+    filter_state = None
 
-    # Compute the number of epochs in "buffer_length"
+    # Initialize band power buffer
     n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) / SHIFT_LENGTH + 1))
-
-    # Initialize the band power buffer (for plotting)
-    # bands will be ordered: [delta, theta, alpha, beta]
     band_buffer = np.zeros((n_win_test, 4))
 
-    """ 3. GET DATA """
-
-    # Set up the plot
-    plt.ion()  # Enable interactive mode
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+    # Set up plots
+    plt.ion()
+    fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(15, 12))
 
     times = np.zeros(int(fs * BUFFER_LENGTH))
+
+    # EEG plot
     (eeg_plot,) = ax1.plot(times, eeg_buffer[:, 0], "-b", linewidth=1)
     ax1.set_title("Raw EEG Signal")
     ax1.set_xlabel("Time (s)")
     ax1.set_ylabel("EEG Amplitude")
 
+    # Band power plot
     band_names = ["Delta", "Theta", "Alpha", "Beta"]
     bar_plot = ax2.bar(band_names, np.zeros(4))
     ax2.set_title("Band Powers")
     ax2.set_ylabel("Power")
 
-    # The try/except structure allows to quit the while loop by aborting the
-    # script with <Ctrl-C>
+    # PPG plot
+    ppg_labels = ["PPG1", "PPG2", "PPG3", "PPG4", "PPG5"]
+    ppg_plot = []
+    for i in range(5):
+        (line,) = ax3.plot(times, ppg_buffer[:, i], label=ppg_labels[i])
+    ppg_plot.append(line)
+    ax3.set_title("PPG Signals")
+    ax3.set_xlabel("Time (s)")
+    ax3.set_ylabel("PPG Amplitude")
+    ax3.legend()
+
+    # Accelerometer plot
+    acc_labels = ["ACC1", "ACC2", "ACC3", "ACC4", "ACC5"]
+    acc_plot = []
+    for i in range(5):
+        (line,) = ax4.plot(times, acc_buffer[:, i], label=f"Acc {acc_labels[i]}")
+        acc_plot.append(line)
+    ax4.set_title("Accelerometer")
+    ax4.set_xlabel("Time (s)")
+    ax4.set_ylabel("Acceleration (g)")
+    ax4.legend()
+
+    # Gyroscope plot
+    gyro_plot = []
+    for i in range(3):
+        (line,) = ax5.plot(times, gyro_buffer[:, i], label=f"Gyro {acc_labels[i]}")
+        gyro_plot.append(line)
+    ax5.set_title("Gyroscope")
+    ax5.set_xlabel("Time (s)")
+    ax5.set_ylabel("Angular Velocity (deg/s)")
+    ax5.legend()
+
+    fig.tight_layout()
+
     print("Press Ctrl-C in the console to break the while loop.")
 
     try:
-        # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
         while True:
-
-            """3.1 ACQUIRE DATA"""
-            # Obtain EEG data from the LSL stream
+            # Get EEG data
             eeg_data, timestamp = inlet.pull_chunk(
                 timeout=1, max_samples=int(SHIFT_LENGTH * fs)
             )
-
-            # Only keep the channel we're interested in
             ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
-
-            # Update EEG buffer with the new data
             eeg_buffer, filter_state = utils.update_buffer(
                 eeg_buffer, ch_data, notch=True, filter_state=filter_state
             )
 
-            """ 3.2 COMPUTE BAND POWERS """
-            # Get newest samples from the buffer
-            data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs)
+            # Get additional sensor data
+            ppg_data, ppg_timestamp = inlet.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs)
+            )
+            acc_data, acc_timestamp = inlet.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs)
+            )
+            gyro_data, gyro_timestamp = inlet.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs)
+            )
 
             # Compute band powers
+            data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs)
             band_powers = utils.compute_band_powers(data_epoch, fs)
             band_buffer, _ = utils.update_buffer(band_buffer, np.asarray([band_powers]))
-            # Compute the average band powers for all epochs in buffer
-            # This helps to smooth out noise
             smooth_band_powers = np.mean(band_buffer, axis=0)
 
-            # Update the plots
+            # Update plots
             times = np.linspace(0, BUFFER_LENGTH, int(fs * BUFFER_LENGTH))
+
+            # Update EEG plot
             eeg_plot.set_ydata(eeg_buffer[:, 0])
             eeg_plot.set_xdata(times)
-            ax1.relim()
-            ax1.autoscale_view()
 
+            # Update band power plot
             for rect, val in zip(bar_plot, smooth_band_powers):
                 rect.set_height(val)
-            ax2.relim()
-            ax2.autoscale_view()
+
+            # Update PPG plot
+            if ppg_data:
+                ppg_buffer = np.roll(ppg_buffer, -len(ppg_data), axis=0)
+                ppg_buffer[-len(ppg_data) :] = ppg_data
+                for i, line in enumerate(ppg_plot):
+                    line.set_ydata(ppg_buffer[:, i])
+                    line.set_xdata(times)
+
+            # Update accelerometer plot
+            if acc_data:
+                acc_buffer = np.roll(acc_buffer, -len(acc_data), axis=0)
+                acc_buffer[-len(acc_data) :] = acc_data
+                for i, line in enumerate(acc_plot):
+                    line.set_ydata(acc_buffer[:, i])
+                    line.set_xdata(times)
+
+            # Update gyroscope plot
+            if gyro_data:
+                gyro_buffer = np.roll(gyro_buffer, -len(gyro_data), axis=0)
+                gyro_buffer[-len(gyro_data) :] = gyro_data
+                for i, line in enumerate(gyro_plot):
+                    line.set_ydata(gyro_buffer[:, i])
+                    line.set_xdata(times)
+
+            # Update plot limits
+            for ax in [ax1, ax2, ax3, ax4, ax5]:
+                ax.relim()
+                ax.autoscale_view()
 
             plt.pause(0.1)
 
+            # Print metrics
             print(
-                "Delta: ",
-                band_powers[Band.Delta],
-                " Theta: ",
-                band_powers[Band.Theta],
-                " Alpha: ",
-                band_powers[Band.Alpha],
-                " Beta: ",
-                band_powers[Band.Beta],
+                f"Delta: {band_powers[Band.Delta]:.4f} Theta: {band_powers[Band.Theta]:.4f}"
+            )
+            print(
+                f"Alpha: {band_powers[Band.Alpha]:.4f} Beta: {band_powers[Band.Beta]:.4f}"
             )
 
-            """ 3.3 COMPUTE NEUROFEEDBACK METRICS """
-            # These metrics could also be used to drive brain-computer interfaces
-
-            # Alpha Protocol:
-            # Simple redout of alpha power, divided by delta waves in order to rule out noise
+            # Compute neurofeedback metrics
             alpha_metric = (
                 smooth_band_powers[Band.Alpha] / smooth_band_powers[Band.Delta]
             )
-            print("Alpha Relaxation: ", alpha_metric)
-
-            # Beta Protocol:
-            # Beta waves have been used as a measure of mental activity and concentration
-            # This beta over theta ratio is commonly used as neurofeedback for ADHD
             beta_metric = smooth_band_powers[Band.Beta] / smooth_band_powers[Band.Theta]
-            print("Beta Concentration: ", beta_metric)
-
-            # Alpha/Theta Protocol:
-            # This is another popular neurofeedback metric for stress reduction
-            # Higher theta over alpha is supposedly associated with reduced anxiety
             theta_metric = (
                 smooth_band_powers[Band.Theta] / smooth_band_powers[Band.Alpha]
             )
-            print("Theta Relaxation: ", theta_metric)
+
+            print(f"Alpha Relaxation: {alpha_metric:.4f}")
+            print(f"Beta Concentration: {beta_metric:.4f}")
+            print(f"Theta Relaxation: {theta_metric:.4f}")
+
+            if ppg_data:
+                print(f"PPG Values: {np.mean(ppg_buffer[-len(ppg_data):], axis=0)}")
+            if acc_data:
+                print(f"Accelerometer: {np.mean(acc_buffer[-len(acc_data):], axis=0)}")
+            if gyro_data:
+                print(f"Gyroscope: {np.mean(gyro_buffer[-len(gyro_data):], axis=0)}")
+            print("\n")
 
     except KeyboardInterrupt:
         print("Closing!")
--- startMuseStream.py
+++ startMuseStream.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     if not muses:
         print("No Muses found")
     else:
-        stream(muses[0]["address"])
+        stream(muses[0]['address'], ppg_enabled=True, acc_enabled=True, gyro_enabled=True)
 
         # Note: Streaming is synchronous, so code here will not execute until the stream has been closed
         print("Stream has ended")
