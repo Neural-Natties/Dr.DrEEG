3ec5241e63a68866f84eb197fa3bc4f685571af2
Tammy Young <tammy.q.young@gmail.com>
2024-11-16 15:02:22 -0700

    make pair page prettier

--- frontend/src/pages/pair.tsx
+++ frontend/src/pages/pair.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useRouter } from "next/router";
+import { useWebSocket } from '@/hooks/useWebSocket';
 
 const PairingPage = () => {
 
   const router = useRouter();
+  const { isConnected } = useWebSocket('ws://localhost:8000/ws');
 
   return (
-    <div className="relative z-[1] h-screen overflow-auto py-4">
-      <div className="flex flex-col items-center h-screen p-6">
-        <h1 className="text-4xl font-bold text-black mb-6">Set Up Your Muse-S Device</h1>
-        <p className="text-xl text-gray-700 mb-8 text-center max-w-2xl">
+    <div className='flex flex-col justify-center z-[1] items-center h-screen !overflow-y-scroll'>
+      <div className='flex flex-col justify-center text-center items-center pt-4'>
+        <h1 className="text-4xl font-bold text-black mb-6 w-full">Set Up Your Muse-S Device</h1>
+        <p className="text-xl text-gray-700 mb-4 text-center max-w-2xl justify-center">
           Follow these simple steps to connect your Muse-S device to Dr. DrEEG and start enjoying personalized karaoke!
         </p>
-
+      </div>
+      <div className="relative flex flex-row z-[1]">
+      <div className="flex flex-col items-center p-6">
         <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-xl">
           <h2 className="text-2xl font-semibold text-blue-600 mb-4">Step 1: Turn On Your Muse-S</h2>
           <p className="text-lg text-gray-700 mb-6">
@@ -40,16 +44,20 @@ const PairingPage = () => {
 
           <h2 className="text-2xl font-semibold text-blue-600 mb-4">Step 5: Start Singing!</h2>
           <p className="text-lg text-gray-700 mb-6">
-            With your Muse-S connected, return to Dr. DrEEG to begin your karaoke experience. Your brain waves will
+            With your Muse-S connected, return to Dr. DrEEG and click <span className='text-blue-600 font-semibold'>Start Karaoke</span> to begin your karaoke experience. Your brain waves will
             guide the song selection, ensuring you get the perfect song for your mood.
           </p>
         </div>
-        <div>
-          <button className="mt-6 px-6 py-2 bg-blue-500 text-white text-xl rounded hover:bg-blue-600" onClick={() => router.push('/karaoke')}>
+      </div>
+      <div className='flex flex-col items-center p-6'>
+        <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-xl">
+          <p>Status: {isConnected? 'Connected' : 'Not Connected'}</p>
+          <button className="mt-6 px-6 py-2 bg-blue-500 text-white text-xl rounded hover:bg-blue-600 disabled:bg-neutral-300" disabled={isConnected ? false : true} onClick={() => router.push('/karaoke')}>
             Start Karaoke
           </button>
         </div>
       </div>
+    </div>
     </div>
   );
 };
