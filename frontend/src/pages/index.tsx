751bf66ae82cebba2b0c9e965a5c26aef981b48b
Tammy Young <tammy.q.young@gmail.com>
2024-11-15 18:27:43 -0700

    pretty background

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,7 +5,7 @@ const HomePage: React.FC = () => {
   const { data, isConnected } = useWebSocket("ws://localhost:8000/wstest");
 
   return (
-    <div className='p-6 text-2xl'>
+    <div className='p-6 text-2xl w-screen h-screen'>
       <h1>WebSocket Messages</h1>
       <p>Status: {isConnected ? 'Connected' : 'Disconnected'}</p>
       <ul>
