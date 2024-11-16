70a8d3b4efca5334068c20c7aeac60fa6faf28ce
Nandan <nandanramesh123@gmail.com>
2024-11-16 02:32:44 -0700

    fix: muse async issues

--- backend/muse/processor.py
+++ backend/muse/processor.py
@@ -1,23 +1,13 @@
 import numpy as np
 from pylsl import StreamInlet, resolve_byprop
-from muselsl import stream, list_muses
 from ml.features import extract_eeg_features
 
 
 class MuseProcessor:
     def __init__(self):
-        self.start_stream()
         self.inlet = self._connect_muse()
         self.buffer_size = 256 * 5  # 5 seconds of data at 256Hz
-        self.eeg_buffer = np.zeros((self.buffer_size, 4))
-
-    def start_stream(self):
-        muses = list_muses()
-        if not muses:
-            raise RuntimeError("No Muse devices found!")
-        stream(
-            muses[0]["address"], ppg_enabled=True, acc_enabled=True, gyro_enabled=True
-        )
+        self.eeg_buffer = np.zeros((self.buffer_size, 5))
 
     def _connect_muse(self):
         streams = resolve_byprop("type", "EEG", timeout=2)
