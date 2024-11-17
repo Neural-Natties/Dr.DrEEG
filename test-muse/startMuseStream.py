9bce8050e60467acbd41baa65de5750b2a8e4f3a
Nandan <nandanramesh123@gmail.com>
2024-11-16 20:59:42 -0700

    chore: add new recording

--- test-muse/startMuseStream.py
+++ test-muse/startMuseStream.py
@@ -10,6 +10,9 @@ from muselsl import stream, list_muses
 if __name__ == "__main__":
 
     muses = list_muses()
+    muses = [m for m in muses if m["name"] == "MuseS-79AA"]
+
+    # Found device MuseS-79AA, MAC Address 25770FE7-C2E5-8082-FCD6-22319FD37812
 
     if not muses:
         print("No Muses found")
