b1daf9e49e100992d73c3484a10ee3cbd005dacb
ethanchung936 <ethanchung936@gmail.com>
2024-11-16 19:25:56 -0700

    add web player

---
+++ frontend/src/components/WebPlayback.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+
+const defaultTrack = {
+    name: "No track playing",
+    album: {
+        images: [
+            { url: "https://via.placeholder.com/150" } // Placeholder image URL
+        ]
+    },
+    artists: [
+        { name: "Unknown artist" }
+    ]
+};
+
+const track = {
+    name: "",
+    album: {
+        images: [
+            { url: "" }
+        ]
+    },
+    artists: [
+        { name: "" }
+    ]
+}
+
+interface WebPlaybackProps {
+    token: string;
+}
+
+const WebPlayback: React.FC<WebPlaybackProps> = ({ token }) => {
+    const [is_paused, setPaused] = useState(false);
+    const [is_active, setActive] = useState(false);
+    const [player, setPlayer] = useState<Spotify.Player | undefined>(undefined);
+    const [current_track, setTrack] = useState(track);
+
+    useEffect(() => {
+
+        const script = document.createElement("script");
+        script.src = "https://sdk.scdn.co/spotify-player.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        window.onSpotifyWebPlaybackSDKReady = () => {
+
+            const player = new window.Spotify.Player({
+                name: 'Web Playback SDK 2',
+                getOAuthToken: cb => { cb(token); },
+                volume: 0.5
+            });
+
+            setPlayer(player);
+
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+                fetch('https://api.spotify.com/v1/me/player', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        device_ids: [device_id],
+                        play: true
+                    })    
+                });
+
+            });
+
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+                setActive(false);
+            });
+
+            player.addListener('player_state_changed', ( state => {
+                console.log(state);
+                if (!state) {
+                    return;
+                }
+
+                setTrack(state.track_window.current_track);
+                setPaused(state.paused);
+
+                player.getCurrentState().then( state => { 
+                    (!state)? setActive(false) : setActive(true) 
+                });
+
+            }));
+
+            player.connect();
+
+        };
+    }, []);
+
+    if (!is_active || !player || !current_track) { 
+        return (
+            <>
+                <div className="container">
+                    <div className="main-wrapper">
+                        <b> Instance not active. Transfer your playback using your Spotify app </b>
+                    </div>
+                </div>
+            </>)
+    } else {
+        return (
+            <>
+                <div className="container">
+                    <div className="main-wrapper">
+
+                        <img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
+
+                        <div className="now-playing__side">
+                            <div className="now-playing__name">{current_track.name}</div>
+                            <div className="now-playing__artist">{current_track.artists[0].name}</div>
+                        
+                            <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
+                                &lt;&lt;
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
+                                { is_paused ? "PLAY" : "PAUSE" }
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
+                                &gt;&gt;
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default WebPlayback
--- frontend/src/pages/karaoke.tsx
+++ frontend/src/pages/karaoke.tsx
@@ -1,19 +1,18 @@
 import { useWebSocket } from '@/hooks/useWebSocket';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSpotifyAuth } from '@/hooks/useAuth'
+import WebPlayback from '@/components/WebPlayback';
+
 
 const KaraokePage: React.FC = () => {
   const { data, isConnected } = useWebSocket('ws://localhost:8000/ws');
-
-  const token = useSpotifyAuth()
+  const token = useSpotifyAuth().token!;
 
   return (
     <div className='p-6 text-2xl w-screen h-screen'>
       <h1>WebSocket Messages</h1>
       <p>Status: {isConnected ? 'Connected' : 'Disconnected'}</p>
-      <ul>
-        <li>{JSON.stringify(data)}</li>
-      </ul>
+      {!token ? <p>loading</p> : <WebPlayback token={token} />}
     </div>
   );
 };
