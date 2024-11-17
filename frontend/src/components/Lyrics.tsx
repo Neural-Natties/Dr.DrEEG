ee35269298e40344abbf67cf2969cd457c46d695
Nandan <nandanramesh123@gmail.com>
2024-11-16 20:17:41 -0700

    feat: display animated music lyrics

---
+++ frontend/src/components/Lyrics.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+interface LyricsProps {
+  lyrics: string[];
+  interval?: number;
+}
+
+export const Lyrics: React.FC<LyricsProps> = ({ lyrics, interval = 3000 }) => {
+  const [currentLyricIndex, setCurrentLyricIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (lyrics?.length) {
+        setCurrentLyricIndex((prev) =>
+          prev + 1 >= lyrics.length ? 0 : prev + 1
+        );
+      }
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [lyrics, interval]);
+
+  return (
+    <div className='h-64 flex flex-col items-center justify-center'>
+      {lyrics
+        ?.slice(currentLyricIndex, currentLyricIndex + 5)
+        .map((line, index) => (
+          <p
+            key={currentLyricIndex + index}
+            className='text-3xl text-center my-4 transition-all duration-500'
+            style={{
+              opacity: index === 2 ? 1 : 0.3,
+              transform: `scale(${index === 2 ? 1.1 : 1})`,
+              color: index === 2 ? '#ffffff' : '#808080',
+            }}
+          >
+            {line}
+          </p>
+        ))}
+    </div>
+  );
+};
--- frontend/src/pages/karaoke.tsx
+++ frontend/src/pages/karaoke.tsx
@@ -1,17 +1,28 @@
-import { useWebSocket } from '@/hooks/useWebSocket';
-import React, { useEffect, useState } from 'react';
-import { useSpotifyAuth } from '@/hooks/useAuth';
+import { Lyrics } from '@/components/Lyrics';
 import WebPlayback from '@/components/WebPlayback';
-
+import { useSpotifyAuth } from '@/hooks/useAuth';
+import { useWebSocket } from '@/hooks/useWebSocket';
 
 const KaraokePage: React.FC = () => {
-  const { data, isConnected } = useWebSocket('ws://localhost:8000/ws');
+  const { data } = useWebSocket('ws://localhost:8000/ws');
   const token = useSpotifyAuth().token!;
 
   return (
-    <div className='p-6 text-2xl w-screen h-screen'>
-      <h1>WebSocket Messages</h1>
-      <p>Status: {isConnected ? 'Connected' : 'Disconnected'}</p>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-black text-white p-6 relative'>
+      {data?.song && (
+        <>
+          <img
+            src={data.song.albumArt || undefined}
+            alt='Album Art'
+            className='w-[24rem] h-[24rem] rounded-2xl shadow-2xl mb-5 z-10'
+          />
+          <h2 className='text-2xl font-bold mb-2 z-10'>{data.song.name}</h2>
+          <p className='text-xl mb-12 text-gray-300 z-10'>{data.song.artist}</p>
+          <div className='z-10 w-full max-w-4xl'>
+            <Lyrics lyrics={data.song.lyrics} />
+          </div>
+        </>
+      )}
       {!token ? <p>loading</p> : <WebPlayback token={token} />}
     </div>
   );
