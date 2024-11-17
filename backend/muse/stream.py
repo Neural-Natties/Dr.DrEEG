5c87ba5a4bb1db935a75383feb9f92b4e552f3cf
Nandan <nandanramesh123@gmail.com>
2024-11-17 06:26:09 -0700

    chore: more cleanup

--- backend/muse/stream.py
+++ backend/muse/stream.py
@@ -3,7 +3,7 @@ from muselsl import stream, list_muses
 if __name__ == "__main__":
     muses = list_muses()
     muses = [m for m in muses if m["name"] == "MuseS-79AA"]
-    
+
     if not muses:
         print("No Muses found")
     else:
