336d0dabc47e73e887edee13928df01cade83559
Nandan-18 <nandanramesh123@gmail.com>
2024-12-23 23:41:51 -0700

    feat: setup frontend config for gh actions

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/Dr.DrEEG' : '',
 };
 
 module.exports = nextConfig;
